cc358e15834fcf97ec989d8200fd1fd60c913938
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-04-11 03:57:05 +0530

    Create hehe.py

---
+++ hehe.py
@@ -0,0 +1,25 @@
+Website-frontend
+    {
+        Introduction
+        ISS Live Location & Notification Widget
+        Astronomy Picture of the Day (APOD)
+        Mars Picture of the Week (MPOW)
+        People in Space
+        Redirects to notification systems
+    }
+    
+Website-Backend
+    {
+        Flask API for frontend
+        APOD refresh script
+        MPOW refresh script
+        People in space refresh script
+        Notification-cum-email system
+        MySQL for handling user data
+    }
+
+Notification-Systems
+    {
+        Page for adding user email and password
+        Page for unsubscribing the notification system
+    }
