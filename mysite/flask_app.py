ed9ea9a975e13bc5bfac3f813ac7012f663a33f7
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-04-22 19:55:11 +0530

    Flask file updated

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -7,3 +7,12 @@ app = Flask(__name__, template_folder='')
 @app.route('/')
 def form():
     return render_template('index.html')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+@app.route('/email-notif.html')
+def sub():
+    return render_template('email-notif.html')
+@app.route('/email-notif1.html')
+def sub1():
+    return render_template('email-notif1.html')
