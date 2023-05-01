280e09269595c62977f08acff9a74d7a69977418
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-05-01 19:30:51 +0530

    Added support for 0 passes

--- isspasses.py
+++ isspasses.py
@@ -23,63 +23,75 @@ def API():
         passes = result['info']['passescount']
         print("Name of the satellite: ",satname)
         print("Number of passes in 10 days: ",passes)
-        for x in range(0,passes):
-            startAz = result['passes'][x]['startAz']
-            startAzCompass = result['passes'][x]['startAzCompass']
-            startUTC = result['passes'][x]['startUTC']
-            maxAz = result['passes'][x]['maxAz']
-            maxAzCompass = result['passes'][x]['maxAzCompass']
-            maxEl =  result['passes'][x]['maxEl']
-            maxUTC = result['passes'][x]['maxUTC']
-            endAz = result['passes'][x]['endAz']
-            endAzCompass = result['passes'][x]['endAzCompass']
-            endUTC = result['passes'][x]['endUTC']
-            mag = result['passes'][x]['mag']
-            duration = result['passes'][x]['duration']
-            startVisibility = result['passes'][x]['startVisibility']
+        if passes > 0:
+            for x in range(0,passes):
+                startAz = result['passes'][x]['startAz']
+                startAzCompass = result['passes'][x]['startAzCompass']
+                startUTC = result['passes'][x]['startUTC']
+                maxAz = result['passes'][x]['maxAz']
+                maxAzCompass = result['passes'][x]['maxAzCompass']
+                maxEl =  result['passes'][x]['maxEl']
+                maxUTC = result['passes'][x]['maxUTC']
+                endAz = result['passes'][x]['endAz']
+                endAzCompass = result['passes'][x]['endAzCompass']
+                endUTC = result['passes'][x]['endUTC']
+                mag = result['passes'][x]['mag']
+                duration = result['passes'][x]['duration']
+                startVisibility = result['passes'][x]['startVisibility']
 
-            print("\nPass no." , x+1)
-            print("Start Azimuth: ",startAz)
-            print("Start Azimuth Compass: ",startAzCompass)
-            print("Start Time (Epoch): ", startUTC)
-            print("Start Time (UTC): ",datetime.datetime.fromtimestamp(startUTC))
-            print("Max Azimuth: ", maxAz)
-            print("Max Azimuth Compass: ",maxAzCompass)
-            print("Max Elevation: ",maxEl)
-            print("Max Time (Epoch): ",maxUTC)
-            print("Max Time (UTC): ",datetime.datetime.fromtimestamp(maxUTC))
-            print("End Azimuth: ",endAz)
-            print("End Azimuth Compass: ",endAzCompass)
-            print("End Time (Epoch): ",endUTC)
-            print("End Time (UTC): ",datetime.datetime.fromtimestamp(endUTC))
-            print("Magnitude: "+ str(mag))
-            print("Pass Duration: ",duration)
-            print("Start Visibility (UTC): ",datetime.datetime.fromtimestamp(startVisibility))
+                print("\nPass no." , x+1)
+                print("Start Azimuth: ",startAz)
+                print("Start Azimuth Compass: ",startAzCompass)
+                print("Start Time (Epoch): ", startUTC)
+                print("Start Time (UTC): ",datetime.datetime.fromtimestamp(startUTC))
+                print("Max Azimuth: ", maxAz)
+                print("Max Azimuth Compass: ",maxAzCompass)
+                print("Max Elevation: ",maxEl)
+                print("Max Time (Epoch): ",maxUTC)
+                print("Max Time (UTC): ",datetime.datetime.fromtimestamp(maxUTC))
+                print("End Azimuth: ",endAz)
+                print("End Azimuth Compass: ",endAzCompass)
+                print("End Time (Epoch): ",endUTC)
+                print("End Time (UTC): ",datetime.datetime.fromtimestamp(endUTC))
+                print("Magnitude: "+ str(mag))
+                print("Pass Duration: ",duration)
+                print("Start Visibility (UTC): ",datetime.datetime.fromtimestamp(startVisibility))
 
+                file = open("iss_passes.txt", "a")
+                print("Name of the satellite: ",satname)
+                print("Number of passes in 10 days: ",passes)
+                file.write("\n\nPass no."+str(x+1))
+                file.write("\nStart Azimuth: "+str(startAz))
+                file.write("\nStart Azimuth Compass: "+str(startAzCompass))
+                file.write("\nStart Time (Epoch): "+str(startUTC))
+                file.write("\nStart Time (UTC): "+str(datetime.datetime.fromtimestamp(startUTC)))
+                file.write("\nMax Azimuth: "+str(maxAz))
+                file.write("\nMax Azimuth Compass: "+str(maxAzCompass))
+                file.write("\nMax Elevation: "+str(maxEl))
+                file.write("\nMax Time (Epoch): "+str(maxUTC))
+                file.write("\nMax Time (UTC): "+str(datetime.datetime.fromtimestamp(maxUTC)))
+                file.write("\nEnd Azimuth: "+str(endAz))
+                file.write("\nEnd Azimuth Compass: "+str(endAzCompass))
+                file.write("\nEnd Time (Epoch): "+str(endUTC))
+                file.write("\nEnd Time (UTC): "+str(datetime.datetime.fromtimestamp(endUTC)))
+                file.write("\nMagnitude: "+str(mag))
+                file.write("\nPass Duration: "+str(duration))
+                file.write("\nStart Visibility (UTC): "+str(datetime.datetime.fromtimestamp(startVisibility)))
+                t = time.localtime()
+                current_time = time.strftime("%H:%M:%S", t)
+                file.write ("\nCurrent Time: "+str(current_time))
+                print("Current Time: ", current_time)
+                file.close()
+            path_current="./iss_passes_current.txt"
+            shutil.move("./iss_passes.txt", path_current)
+            time.sleep(5)
+            t2.start()
+        else:
+            print("No visible ISS passes this week. Stay tuned for next week!")
             file = open("iss_passes.txt", "a")
             print("Name of the satellite: ",satname)
             print("Number of passes in 10 days: ",passes)
-            file.write("\n\nPass no."+str(x+1))
-            file.write("\nStart Azimuth: "+str(startAz))
-            file.write("\nStart Azimuth Compass: "+str(startAzCompass))
-            file.write("\nStart Time (Epoch): "+str(startUTC))
-            file.write("\nStart Time (UTC): "+str(datetime.datetime.fromtimestamp(startUTC)))
-            file.write("\nMax Azimuth: "+str(maxAz))
-            file.write("\nMax Azimuth Compass: "+str(maxAzCompass))
-            file.write("\nMax Elevation: "+str(maxEl))
-            file.write("\nMax Time (Epoch): "+str(maxUTC))
-            file.write("\nMax Time (UTC): "+str(datetime.datetime.fromtimestamp(maxUTC)))
-            file.write("\nEnd Azimuth: "+str(endAz))
-            file.write("\nEnd Azimuth Compass: "+str(endAzCompass))
-            file.write("\nEnd Time (Epoch): "+str(endUTC))
-            file.write("\nEnd Time (UTC): "+str(datetime.datetime.fromtimestamp(endUTC)))
-            file.write("\nMagnitude: "+str(mag))
-            file.write("\nPass Duration: "+str(duration))
-            file.write("\nStart Visibility (UTC): "+str(datetime.datetime.fromtimestamp(startVisibility)))
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
-            file.write ("\nCurrent Time: "+str(current_time))
-            print("Current Time: ", current_time)
+            file.write("No visible ISS passes this week. Stay tuned for next week!")
             file.close()
         path_current="./iss_passes_current.txt"
         shutil.move("./iss_passes.txt", path_current)
@@ -94,7 +106,7 @@ def Mail_Trigger():
     # get day of week as an integer
     x = dt.weekday()
     print('Day of the week is:', x)
-    if x == 5:
+    if x == 0:
         rawpath = os.getcwd() + "\\mail.py"
         path = rawpath.replace('\\', '/')
         subprocess.call(['python', path])
