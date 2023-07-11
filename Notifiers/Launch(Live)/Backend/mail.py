0a57f5a91e2ef35b1095941f812e160a395860d6
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-07-11 20:41:45 +0530

    Update mail.py
    Solved privacy issue

--- Notifiers/Launch(Live)/Backend/mail.py
+++ Notifiers/Launch(Live)/Backend/mail.py
@@ -32,38 +32,38 @@ with open("./emails_current.txt") as f:
     #print (body)
     emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", body)
     # print (emails)
-
-sender_email = "crudespace@gmail.com"
-receiver_email = "vedantfar@gmail.com"
-password = ""
-cc = ""
-bcc = emails
-# bcc = "farkadevedant@gmail.com, varospaxo@gmail.com"
-
-with open('./launches_Notifier_OP.txt') as file:
-  sub = file.readline().strip('Mission Name: ')
-  # print (sub)
-
-message = MIMEMultipart("alternative")
-subject = "Next Launch "+sub+" in one hour!"
-message["Subject"] = subject
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Cc"] = cc
-message["Bcc"] = ", ".join(bcc)
-# message["Bcc"] = bcc
-# print (message["Bcc"])
-
-# string to store the body of the mail
-with open('./launches_Notifier_OP.txt') as f:
-    body = f.read()
-
-# attach the body with the msg instance
-message.attach(MIMEText(body, 'plain'))
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        message["From"], message["To"].split(",") + message["Cc"].split(",") + message["Bcc"].split(","), message.as_string()
-    )
+for emails in email:
+  sender_email = "crudespace@gmail.com"
+  receiver_email = email
+  password = ""
+  cc = ""
+  bcc = ""
+  # bcc = "farkadevedant@gmail.com, varospaxo@gmail.com"
+  
+  with open('./launches_Notifier_OP.txt') as file:
+    sub = file.readline().strip('Mission Name: ')
+    # print (sub)
+  
+  message = MIMEMultipart("alternative")
+  subject = "Next Launch "+sub+" in one hour!"
+  message["Subject"] = subject
+  message["From"] = sender_email
+  message["To"] = receiver_email
+  message["Cc"] = cc
+  message["Bcc"] = ", ".join(bcc)
+  # message["Bcc"] = bcc
+  # print (message["Bcc"])
+  
+  # string to store the body of the mail
+  with open('./launches_Notifier_OP.txt') as f:
+      body = f.read()
+  
+  # attach the body with the msg instance
+  message.attach(MIMEText(body, 'plain'))
+  # Create secure connection with server and send email
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          message["From"], message["To"].split(",") + message["Cc"].split(",") + message["Bcc"].split(","), message.as_string()
+      )
