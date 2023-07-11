9ed229e5fc3d69d67892d5f644d967dbee5343da
Vedant Farkade <64273563+varospaxo@users.noreply.github.com>
2023-07-11 20:42:56 +0530

    Update issmail.py
    Solved privacy issue

--- Notifiers/ISS(Live)/backend/issmail.py
+++ Notifiers/ISS(Live)/backend/issmail.py
@@ -32,31 +32,31 @@ with open("./hehe_current.txt") as f:
     #print (body)
     emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", body)
     # print (emails)
-
-sender_email = "crudespace@gmail.com"
-receiver_email = "vedantfar@gmail.com"
-password = ""
-cc = ""
-bcc = emails
-
-message = MIMEMultipart("alternative")
-message["Subject"] = "Next 10 Days ISS Visible Passes (Beta)"
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Cc"] = cc
-message["Bcc"] = ", ".join(bcc)
-# print (message["Bcc"])
-
-# string to store the body of the mail
-with open('iss_passes_current.txt') as f:
-    body = f.read()
-
-# attach the body with the msg instance
-message.attach(MIMEText(body, 'plain'))
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(
-        message["From"], message["To"].split(",") + message["Cc"].split(",") + message["Bcc"].split(","), message.as_string()
-    )
+for emails in email:
+  sender_email = "crudespace@gmail.com"
+  receiver_email = email
+  password = ""
+  cc = ""
+  bcc = ""
+  
+  message = MIMEMultipart("alternative")
+  message["Subject"] = "Next 10 Days ISS Visible Passes (Beta)"
+  message["From"] = sender_email
+  message["To"] = receiver_email
+  message["Cc"] = cc
+  message["Bcc"] = ", ".join(bcc)
+  # print (message["Bcc"])
+  
+  # string to store the body of the mail
+  with open('iss_passes_current.txt') as f:
+      body = f.read()
+  
+  # attach the body with the msg instance
+  message.attach(MIMEText(body, 'plain'))
+  # Create secure connection with server and send email
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(
+          message["From"], message["To"].split(",") + message["Cc"].split(",") + message["Bcc"].split(","), message.as_string()
+      )
